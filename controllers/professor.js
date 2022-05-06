65cee5f6987e45625ae23588ca0c706931c338b4
tharunv2122 <tharunvind@gmail.com>
2022-05-06 16:00:51 +0530

    init build

--- controllers/professor.js
+++ controllers/professor.js
@@ -7,8 +7,8 @@ exports.getHome = (req, res) => {
 exports.getQR = async (req, res) => {
     const lecture = req.params.lecture;
     
-    const URL = `https://attendance-iiit.herokuapp.com/mark/${lecture}`;
-
+   const URL = `http://attendance-iiit.herokuapp.com/mark/${lecture}`;
+    //const URL = `http://localhost:1234/mark/${lecture}`
     res.render('make-qr', {url : URL})
 }
 
